0c1ec1a15fe8693fa7fd09450498afadc8386dab
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-03 08:49:19 -0700

    add conditional statement

--- _tests_/aliens.test.js
+++ _tests_/aliens.test.js
@@ -1,4 +1,4 @@
-import { alienQueen, alien, giantQueen, greenAlien } from "../src/aliens.js";
+import {  greyAlienGrown, greyAlienGiant } from "../src/aliens.js";
 
 describe("Aliens", () => {
 
@@ -10,20 +10,20 @@ describe("Aliens", () => {
   //   stateControl();
   // });
 
-  test("should determine if an object is created", () => {
-    expect(alien()).toEqual({ "height": 14 });
-  });
+  // test("should determine if an object is created", () => {
+  //   expect(alien()).toEqual({ "height": 14 });
+  // });
 
-  test("should determine the supreme dominance of the queen", () => {
-    expect(alienQueen.height).toEqual(7);
-  });
+  // test("should determine the supreme dominance of the queen", () => {
+  //   expect(greyAlienGrown.height).toEqual(7);
+  // });
+
+  // test("should grow the height of the queen by 2 times", () => {
+  //   expect(greyAlienGrown.height).toEqual(14)
+  // });
 
   test("should grow the height of the queen by 2 times", () => {
-    expect(giantQueen.height).toEqual(14)
-  });
-
-  test("should grow the height of the queen by 2 times", () => {
-    expect(greenAlien.height).toEqual(7)
+    expect(greyAlienGiant.height).toEqual(8)
   });
 
 });
--- src/aliens.js
+++ src/aliens.js
@@ -21,54 +21,31 @@ export const changeState = (prop) => { //adds abilities to the things (made with
   };
 };
 
-export const multiplyState = (prop) => { //adds abilities to the things (made with code above) // property //
-  // soil is property
-
+export const multiplyState = (prop) => {
   return (value) => {
-    // 5 is passed in here
-      return (state) => ({
-        //
-        ...state, // spread operator, creating a new version, state -
-        [prop]: (state[prop] || 0) * value,
-      });
+      return (state) => {
+        if (state[prop] < 100000) {
+            return {
+          ...state,
+          [prop]: (state[prop] || 0) + 100, 
+            }
+          }
+      };
+      
   };
 };
 
-export const multiplyForever = (thing) => {
-  if (thing.height >= 100) {
-    return thing;
-  } else {
-    return multiplyForever(thing.height * 2);
-  }
-}
-
-// 
-
-// export const multiplyForever = (thing) => {
-//   // This is the termination condition.
-//   if (isNaN(thing)) {
-//     return;
-//   }
-//   if (thing >= 100) {  // base
-//     return thing;
-//   } else {
-//     return multiplyForever(thing * 2); // recursion
-//   }
-// }
-
-
 export const human = storeState();
+export const greyAlien = storeState();
+export const greenAlien= storeState();
 
 export const alienHeight = changeState("height")(7);
-export const alienGrowth = multiplyState("height")(2);
+export const alientGrowth = multiplyState("height")(2);
 
-export const alien = storeState();
-export const alien2 = storeState();
 
-export const greenAlien = alien2(alienHeight);
+export const greyAlienGrown = greyAlien(alienHeight);
+export const greyAlienGiant = greyAlien(alientGrowth);
 
-export const alienQueen = alien(alienHeight);
-export const giantQueen = alien(alienGrowth);
 
 
 
